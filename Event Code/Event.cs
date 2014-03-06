8accb307e82a19ee302cf9713e433b6ae632d888
matthousley <valide_celand@hotmail.com>
2014-03-06 09:44:43 +0000

    Event Code

---
+++ Event Code/Event.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Event : MonoBehaviour {
+	
+	public GameObject[] prefabToSpawn;
+	public int spawner;
+	public Vector3 spawnLocation = new Vector3 (0,0,0);
+	
+	
+	// Use this for initialization
+	void Start () {	
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if ((StatsCode.minutes == 7) && (StatsCode.seconds == 0))
+		{
+	
+		SpawnEvent();
+			
+		}
+
+		if ((StatsCode.minutes == 4) && (StatsCode.seconds == 0))
+		{
+			
+			SpawnEvent();
+			
+		}
+
+		if ((StatsCode.minutes == 1) && (StatsCode.seconds == 0))
+		{
+			
+			SpawnEvent();
+			
+		}
+	}
+	
+	void SpawnEvent () {					//Creates a chosen object at chosen location when called
+	spawner = Random.Range (0,11);
+   	Instantiate(prefabToSpawn[spawner], spawnLocation, Quaternion.identity );
+	}
+}
---
+++ Event Code/ThirdMidNegative.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdMidNegative : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		StatsCode.player2health =- (int) (StatsCode.player2health*0.50);
+		StatsCode.player2thirst =- (int) (StatsCode.player2thirst*0.30);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Destroy(gameObject, 4);
+	}
+}
