a95bfc3d194ea3f634446dfbd99582250b5c6ea7
Jack James <scunnyrule45@hotmail.co.uk>
2014-03-27 09:58:47 +0000

    FairTradeChocolate
    Added Chocolate and Fairtrade Chocolate to the first world shop

--- Unity Files/New Unity Project 1/Assets/shopFirst.cs
+++ Unity Files/New Unity Project 1/Assets/shopFirst.cs
@@ -32,6 +32,8 @@ public class shopFirst : MonoBehaviour {
 			GUI.Box (new Rect (Screen.width*0.192f, Screen.height*0.30f, Screen.width*0.18f, Screen.height*0.27f), "Buy Goods");
 			GUI.Label  (new Rect (Screen.width*0.21f, Screen.height*0.385f, Screen.width*0.03f, Screen.height*0.03f), "$1.50");
 			GUI.Label  (new Rect (Screen.width*0.27f, Screen.height*0.385f, Screen.width*0.03f, Screen.height*0.03f), "$1.10");
+			GUI.Label  (new Rect (Screen.width*0.21f, Screen.height*0.47f, Screen.width*0.03f, Screen.height*0.03f), "$1.00");
+			GUI.Label  (new Rect (Screen.width*0.27f, Screen.height*0.47f, Screen.width*0.03f, Screen.height*0.03f), "$2.00");
 			if (GUI.Button (new Rect (Screen.width*0.194f, Screen.height*0.33f, Screen.width*0.055f, Screen.height*0.055f), water)) {
 				if(StatsCode.player1money >= 1.50f)
 				{
@@ -54,7 +56,29 @@ public class shopFirst : MonoBehaviour {
 					}else{GUI.Label(new Rect (Screen.width*0.24f, Screen.height*0.3f, Screen.width*0.03f, Screen.height*0.03f), "Already Fed.");}
 				}
 			}
+			if (GUI.Button (new Rect (Screen.width*0.194f, Screen.height*0.41f, Screen.width*0.055f, Screen.height*0.055f), "Chocolate")) {
 
+				if(StatsCode.player1money >= 1.00f)
+				{
+					if(StatsCode.player1hunger < 91){
+						StatsCode.player1hunger+= 10;
+						StatsCode.player1money -= 1.00f;
+						
+					}else{GUI.Label(new Rect (Screen.width*0.24f, Screen.height*0.3f, Screen.width*0.03f, Screen.height*0.03f), "Already Fed.");}
+				}
+			}
+			if (GUI.Button (new Rect (Screen.width*0.255f, Screen.height*0.41f, Screen.width*0.075f, Screen.height*0.055f), "FairTrade Chocolate")) {
+
+				if(StatsCode.player1money >= 1.00f)
+				{
+					if(StatsCode.player1hunger < 91){
+						StatsCode.player1hunger+= 10;
+						StatsCode.player1money -= 2.00f;
+						StatsCode.player2money += 1.00f;
+					
+					}else{GUI.Label(new Rect (Screen.width*0.24f, Screen.height*0.3f, Screen.width*0.03f, Screen.height*0.03f), "Already Fed.");}
+				}
+			}
 		}
 	}
 }
